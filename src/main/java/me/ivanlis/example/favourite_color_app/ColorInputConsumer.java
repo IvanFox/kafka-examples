7ceec279024c3effbaab6afbb891c17d3475bbc7
IvanFox <zvezdo4eet@gmail.com>
2018-03-14 20:41:05 +0200

    [Example 2] Minor Refactoring

--- src/main/java/me/ivanlis/example/favourite_color_app/ColorInputConsumer.java
+++ src/main/java/me/ivanlis/example/favourite_color_app/ColorInputConsumer.java
@@ -1,7 +1,7 @@
 package me.ivanlis.example.favourite_color_app;
 
 import static java.util.Collections.singletonList;
-import static me.ivanlis.example.utils.Utils.createConsumerProperties;
+import static me.ivanlis.example.utils.Utils.createDefaultConsumerProperties;
 
 import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -12,7 +12,7 @@ public class ColorInputConsumer {
     private final static String GROUP_ID = "fav_color_consumer_test";
 
     public static void main(String[] args) {
-        Properties consumerProperties = createConsumerProperties(GROUP_ID, "localhost:9092");
+        Properties consumerProperties = createDefaultConsumerProperties(GROUP_ID, "localhost:9092");
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerProperties);
 
         kafkaConsumer.subscribe(singletonList(ColorProducer.FAV_COLOR_INPUT_TOPIC));
--- src/main/java/me/ivanlis/example/favourite_color_app/CurrentColorConsumer.java
+++ src/main/java/me/ivanlis/example/favourite_color_app/CurrentColorConsumer.java
@@ -2,7 +2,7 @@ package me.ivanlis.example.favourite_color_app;
 
 import static java.util.Collections.singletonList;
 import static me.ivanlis.example.favourite_color_app.ColorCounter.*;
-import static me.ivanlis.example.utils.Utils.createConsumerProperties;
+import static me.ivanlis.example.utils.Utils.createDefaultConsumerProperties;
 
 import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -13,7 +13,7 @@ public class CurrentColorConsumer {
     private final static String GROUP_ID = "fav_current_consumer_test";
 
     public static void main(String[] args) {
-        Properties consumerProperties = createConsumerProperties(GROUP_ID, "localhost:9092");
+        Properties consumerProperties = createDefaultConsumerProperties(GROUP_ID, "localhost:9092");
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerProperties);
 
         kafkaConsumer.subscribe(singletonList(CURRENT_FAV_COLOR_COUNT_OUTPUT_TOPIC));
