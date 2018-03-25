7759e27c6852b6b444aafcd39cccb84199e79935
IvanFox <zvezdo4eet@gmail.com>
2018-03-25 17:05:35 +0300

    [AVRO] Schema evolution example | Refactored

--- src/main/java/me/ivanlis/avro/evolution/SchemaEvolutionDemo.java
+++ src/main/java/me/ivanlis/avro/evolution/SchemaEvolutionDemo.java
@@ -1,7 +1,12 @@
 package me.ivanlis.avro.evolution;
 
+import static java.util.Collections.*;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import me.ivanlis.CustomerV1;
 import me.ivanlis.CustomerV2;
 import org.apache.avro.file.DataFileReader;
@@ -14,17 +19,30 @@ import org.apache.avro.specific.SpecificDatumWriter;
 public class SchemaEvolutionDemo {
 
     public static void main(String[] args) {
+        final String location = "customer_v1.avro";
+        final CustomerV1 customerV1 = createCustomerV1();
+        System.out.println("Customer version 1: " + customerV1);
 
-        CustomerV1 customerV1 = CustomerV1.newBuilder()
+        System.out.println("Persisting customer v1 to a disk");
+        persistCustomerV1(customerV1, location);
+
+        System.out.println("Reading customer v1 with customer v2 schema");
+        List<CustomerV2> customerV2s = readCustomerV2(location);
+        System.out.println(customerV2s);
+
+    }
+
+    private static CustomerV1 createCustomerV1(){
+        return CustomerV1.newBuilder()
                 .setFirstName("Ivan")
                 .setLastName("Fox")
                 .setWeight(75f)
                 .setHeight(178.8f)
                 .setAge(30)
                 .build();
+    }
 
-        System.out.println("Customer version 1: " + customerV1);
-
+    private static void persistCustomerV1(CustomerV1 customerV1, String location) {
         final DatumWriter<CustomerV1> datumWriter = new SpecificDatumWriter<>(CustomerV1.class);
         final DataFileWriter<CustomerV1> dataFileWriter = new DataFileWriter<>(datumWriter);
 
@@ -35,21 +53,19 @@ public class SchemaEvolutionDemo {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Persist customer v1 to disc");
+    }
 
-        System.out.println("Reading customer v1 with customer v2 schema");
-
-
+    private static List<CustomerV2> readCustomerV2(String location) {
         final DatumReader<CustomerV2> datumReader = new SpecificDatumReader<>(CustomerV2.class);
-
+        List<CustomerV2> customersV2 = new ArrayList<>();
         try {
-            final DataFileReader<CustomerV2> dataFileReader = new DataFileReader<CustomerV2>(new File("customer_v1.avro"), datumReader);
+            final DataFileReader<CustomerV2> dataFileReader = new DataFileReader<>(new File(location), datumReader);
             while (dataFileReader.hasNext()) {
-                CustomerV2 customerV2 = dataFileReader.next();
-                System.out.println("Customer v2: " + customerV2);
+                customersV2.add(dataFileReader.next());
             }
+            return customersV2;
         } catch (IOException e) {
-            e.printStackTrace();
+            return emptyList();
         }
     }
 }
